fe9ec602bfa8d642af88f51e7ebd74f666aedd23
Chris Fenton <actech@CC-ACTECH-W.evergreen.edu>
2012-07-03 21:21:15 -0700

    Added thumbnails and excerpts to latest posts section

--- page-test2.php
+++ page-test2.php
@@ -7,15 +7,24 @@ get_header(); ?>
     <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
     <?php roots_main_before(); ?>
       <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
+		<h4>Featured Post</h4>
 		<?php include (TEMPLATEPATH . '/featured.php'); ?>
 		<hr>
+		<h4>Latest Posts</h4>
         <?php roots_loop_before(); ?>
 			<?php $limit = get_option('posts_per_page');
 			query_posts('showposts=' . $limit . '&paged=' . $paged .'&cat=-3'); ?>
-			<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?>
-			<h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-			<?php the_content(); ?>
-			<?php endwhile; endif; ?>
+			<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
+					<div class="seeds_latest">
+						<?php the_post_thumbnail('thumbnail'); ?>
+						<h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
+						<?php roots_entry_meta(); ?>
+						<?php the_excerpt(); ?>
+					</div>
+					<hr>
+				<?php endwhile; 
+			endif; ?>
         <?php roots_loop_after(); ?>
       </div><!-- /#main -->
     <?php roots_main_after(); ?>
