de4ccaa2c1faf959c9328bef301a2dff9a99847a
Chris Fenton <cfenton30@gmail.com>
2012-07-05 00:37:35 -0700

    Worked on header.php

--- inc/custom.php
+++ inc/custom.php
@@ -1,3 +1,10 @@
 <?php
 
 // Custom functions
+// function seeds_logo() {
+//     echo '<object data='img/seeds_logo_opt.svg' >
+//             <img src='img/seeds_logo_opt.png' />
+//         </object>';
+// }
+
+// add_action('logo','seeds_logo');
