d964a6ca2f3e7adbc93ccb6368fbd6d9d2f5ec91
Chris Fenton <actech@CC-ACTECH-W.evergreen.edu>
2012-07-05 19:26:25 -0700

    Worked on latest posts section

--- header.php
+++ header.php
@@ -39,16 +39,12 @@
     <div class="row">
       <div class="span6">
         <a href="<?php bloginfo('url'); ?>" alt="<?php bloginfo('name'); ?>">
-          <div id="logo">
-            <object data='<?php echo get_template_directory_uri(); ?>/img/seeds_logo_opt.svg' >
-                <img src="<?php echo get_template_directory_uri(); ?>/img/seeds_logo_opt.png" />
-             </object>
-           </div>
+            <img src="<?php echo get_template_directory_uri(); ?>/img/seeds_logo_opt.png" />
          </a>
       </div>
       <div class="span6">
         <h3 id="tagline">
-          Tech news for students, by students
+          <?php echo get_bloginfo( 'description' ); ?>
         </h3>
       </div>
     </div><!-- end row 1 -->
