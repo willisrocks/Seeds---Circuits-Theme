3010d74e121134e46609ea56ac0b2d802be8c61b
Chris Fenton <cfenton30@gmail.com>
2012-07-09 00:49:56 -0700

    Worked on entry-meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -2,6 +2,8 @@
 
 // Return post entry meta information
 function roots_entry_meta() {
-  echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate>'. sprintf(__('Posted on %s at %s.', 'roots'), get_the_date(), get_the_time()) .'</time>';
-  echo '<p class="byline author vcard">'. __('Written by', 'roots') .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a></p>';
+  echo '<ul class="entry-meta">';
+  echo '<li><span><time class="updated" datetime="'. get_the_time('c') .'" pubdate>'.'<i class="icon-calendar"> </i> ' . get_the_date() .'</time></span></li>';
+  echo '<li><span class="byline author vcard">' .' <a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'.'<i class="icon-user"> </i> '. get_the_author() .'</a></li>';
+  echo '</ul>';
 }
