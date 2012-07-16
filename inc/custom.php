f02a73df8d1949e22c5f64e6ead41db6e8ff90b3
Chris Fenton <actech@CC-ACTECH-W.evergreen.edu>
2012-07-16 16:10:33 -0700

    Added default thumbnail image for single.php

--- inc/custom.php
+++ inc/custom.php
@@ -18,14 +18,21 @@ function my_get_display_author_posts() {
     global $authordata, $post;
 
     $authors_posts = get_posts( array( 'author' => $authordata->ID, 'showposts' => '2', 'post__not_in' => array( $post->ID ) ) );
+    
 
     $output = '<ul class="latest-single">';
     foreach ( $authors_posts as $authors_post ) {
+        if ( has_post_thumbnail( $authors_post->ID ) ) {
+          $authors_thumb = get_the_post_thumbnail( $authors_post->ID, 'medium' );
+          } else {
+          $authors_thumb = '<img src="' . get_template_directory_uri() . '/img/seeds_logo_placeholder.png" />';
+          }
         $output .= '<li>';
-            $output .= '<a href="' . get_permalink( $authors_post->ID ) . '">' . get_the_post_thumbnail( $authors_post->ID, 'medium' ) . '</a>';
-            $output .= '<div class="latest-overlay">';
-                $output .= '<span><a href="' . get_permalink( $authors_post->ID ) . '">' . apply_filters( 'the_title', $authors_post->post_title, $authors_post->ID ) . '</a></span>';
-            $output .= '</div>';
+        // $output .= '<a href="' . get_permalink( $authors_post->ID ) . '">' . get_the_post_thumbnail( $authors_post->ID, 'medium' ) . '</a>';
+        $output .= '<a href="' . get_permalink( $authors_post->ID ) . '">' . $authors_thumb . '</a>';
+        $output .= '<div class="latest-overlay">';
+        $output .= '<span><a href="' . get_permalink( $authors_post->ID ) . '">' . apply_filters( 'the_title', $authors_post->post_title, $authors_post->ID ) . '</a></span>';
+        $output .= '</div>';
         $output .= '</li>';
     }
     $output .= '</ul>';
--- latest-single.php
+++ latest-single.php
@@ -5,7 +5,15 @@
   $my_query = new WP_Query( array('category_name' => 'article', 'showposts' => '2', 'post__not_in' => array($currentID)));
   while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
     <li>
-      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <?php if ( has_post_thumbnail() ) {
+          the_post_thumbnail('medium');
+          } else { ?>
+          <img src="<?php echo get_template_directory_uri(); ?>/img/seeds_logo_placeholder.png" />
+          <?php } ?>
+
+        <!-- <?php the_post_thumbnail('medium'); ?> -->
+      </a>
       <div class="latest-overlay">
         <span><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></span>
       </div>
