b52e3facc98eb5b012686da8eb87ec4952eb5768
unknown <actech@CC-ACTECH-W.evergreen.edu>
2012-07-25 18:53:54 -0700

    added social icons in navbar

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,9 +7,17 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-      <a class="brand" href="<?php echo home_url(); ?>/">
+      <div class="seeds-social-icons">
+        <ul class="seeds-social">
+          <li><a class="seeds-facebook" href="http://www.facebook.com/evergreencomputercenter" title="Facebook" target="_blank">Facebook</a></li>
+          <li><a class="seeds-twitter" href="http://twitter.com/tesc_compcenter" title="Twitter" target="_blank">Twitter</a></li>
+          <li><a class="seeds-google" href="http://plus.google.com/b/102580020876069865868/102580020876069865868" title="Google Plus" target="_blank">Google Plus</a></li>
+          <li class="seeds-social-last"><a class="seeds-rss" href="<?php bloginfo('rss2_url'); ?>" title="RSS" target="_blank">RSS</a></li>
+        </ul>    
+      </div>
+      <!-- <a class="brand" href="<?php echo home_url(); ?>/">
         <?php bloginfo('name'); ?>
-      </a>
+      </a> -->
       <nav id="nav-main" class="nav-collapse pull-right" role="navigation">
         <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Navbar_Nav_Walker(), 'menu_class' => 'nav')); ?>
       </nav>
