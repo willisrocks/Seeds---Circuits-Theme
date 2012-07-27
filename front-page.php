884b1921471635f9418511d75dc7eaf04f4a8d8a
Chris Fenton <actech@CC-ACTECH-W.evergreen.edu>
2012-07-26 21:05:32 -0700

    added optional short-title and tweaked front page

--- front-page.php
+++ front-page.php
@@ -3,14 +3,14 @@
 <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
   <?php roots_main_before(); ?>
   <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
-    <h4>Featured Post</h4>
+    <h4 class="content-label">Featured Post</h4>
     <?php include (TEMPLATEPATH . '/featured.php'); ?>
   </div><!-- /#main -->
   <?php roots_main_after(); ?>
   <?php roots_sidebar_before(); ?>
   <aside id="sidebar" class="<?php echo SIDEBAR_CLASSES; ?>" role="complementary">
     <?php roots_sidebar_inside_before(); ?>
-    <h4>Latest Posts</h4>
+    <h4 class="content-label">Latest Posts</h4>
     <?php include (TEMPLATEPATH . '/latest.php'); ?>
     <?php roots_sidebar_inside_after(); ?>
   </aside><!-- /#sidebar -->
--- latest.php
+++ latest.php
@@ -4,11 +4,15 @@ $currentID = get_the_ID();
 $my_query = new WP_Query( array('category_name' => 'article', 'showposts' => '5', 'post__not_in' => array($currentID)));
 while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 <div class="seeds_latest">
-  <?php the_post_thumbnail('thumbnail'); ?></span>
-  <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-  <?php roots_entry_meta(); ?>
-  <!-- <?php the_excerpt(); ?> -->
+	<?php the_post_thumbnail('thumbnail'); ?>
+	<?php if (get_post_meta($post->ID, "short-title", true)) { ?>
+        <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php echo get_post_meta($post->ID, "short-title", true); ?></a></h3>
+      <?php } else { ?>
+        <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+      <?php } ?>
+	<?php roots_entry_meta(); ?>
+	<!-- <?php the_excerpt(); ?> -->
 </div>
-<hr>
+<hr />
 <?php endwhile; ?>
 <?php roots_loop_after(); ?>
